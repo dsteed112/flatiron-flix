1fae4ef5082edcfaa562503563a4dc989b67acd2
github username <github email address>
2020-06-22 11:34:52 -0600

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <div className="inner-content">
+          <div className="brand">
+            <Link to="/"><img src="https://fontmeme.com/permalink/200618/c6962e9e4a769fd9f8fed6c4db438122.png" alt="netflix-font" border="0" />
+            </Link>
+          </div>
+
+          <ul className="nav-links">
+            <li>
+              <Link to="/">Watch List</Link>
+            </li>
+
+            <li>
+              <Link to="/watched">Watched</Link>
+            </li>
+
+            <li>
+              <Link to="/add" className="btn btn-main">
+                + Add
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+};
